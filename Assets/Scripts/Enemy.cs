075b7ab7c87b23f988649a7e31d53473737405ba
Alexey <besedinalexey@gmail.com>
2020-03-30 16:08:50 +0300

    Basic patrol

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,76 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+	private Rigidbody2D _enemy;
+	public float patrolLeftBorderX;
+	public float patrolRightBorderX;
+	
 	public Animator animator;
 
 	public int maxHealth = 0;
-	int currentHealth = 100;
+	private int currentHealth = 100;
 
-    // Start is called before the first frame update
+	public float waitTime;
+	public float movementSpeed;
+	private bool _isWalking;
+	private bool _facesRight;
+	private float _currentWaitTime;
+
+	// Start is called before the first frame update
     void Start()
     {
-		maxHealth = currentHealth;  
+		maxHealth = currentHealth;
+		_facesRight = true;
+		_enemy = GetComponent<Rigidbody2D>();
+		_currentWaitTime = waitTime;
+		_isWalking = true;
     }
 
-	public void TakeDamage(int damage)
+    private void Update()
+    {
+	    if (_isWalking)
+	    {
+		    if (_facesRight)
+		    {
+			    if (transform.position.x < patrolRightBorderX)
+			    {
+				    _enemy.velocity = new Vector2(movementSpeed, 0);
+			    }
+			    else
+			    {
+				    _isWalking = false;
+			    }
+		    }
+		    else
+		    {
+			    if (transform.position.x > patrolLeftBorderX)
+			    {
+				    _enemy.velocity = new Vector2(-movementSpeed, 0);
+			    }
+			    else
+			    {
+				    _isWalking = false;
+			    }
+		    }
+	    }
+	    else
+	    {
+		    _currentWaitTime -= Time.deltaTime;
+
+		    if (_currentWaitTime <= 0)
+		    {
+			    _currentWaitTime = waitTime;
+			    Flip();
+			    _isWalking = true;
+		    }
+	    }
+    }
+
+    public void TakeDamage(int damage)
 	{
 		currentHealth -= damage;
 
@@ -30,6 +85,32 @@ public class Enemy : MonoBehaviour
 		animator.SetBool("Dead", true);
 
 		GetComponent<Collider2D>().enabled = false;
-		this.enabled = false;
+		enabled = false;
+	}
+	
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		_facesRight = !_facesRight;
+
+		transform.Rotate(0f, 180f, 0f);
+	}
+	
+	private void Move()
+	{
+		
+		
+		// transform.position += _movement * Time.deltaTime * movementSpeed;
+		//
+		//
+		//
+		// if (_movement.x > 0 && !_facesRight)
+		// {
+		// 	Flip();
+		// }
+		// else if (_movement.x < 0 && _facesRight)
+		// {
+		// 	Flip();
+		// }
 	}
 }
