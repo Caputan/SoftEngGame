0f24314a3d831d2ab78e6e6cb479976e1f69054e
Artem <zmet1999@mail.ru>
2020-03-30 17:10:56 +0300

    ladder climbing

---
+++ Assets/Scripts/InteractionCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionCheck : MonoBehaviour
+{
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Collider2D>().tag == "Player")
+        {
+            Player.GetComponent<Player>().canClimb = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Collider2D>().tag == "Player")
+        {
+            Player.GetComponent<Player>().canClimb = false;
+        }
+    }
+}
