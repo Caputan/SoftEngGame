8052e98a60f3c91a65586799a09a3573236323af
Alexey <besedinalexey@gmail.com>
2020-04-08 13:35:55 +0300

    Docs example

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Скрипт для работы с главным меню и меню настроек
+/// </summary>
 public class Menu : MonoBehaviour
 {
+    /// <summary>
+    /// Ссылка на регулировщик громкости проекта.
+    /// </summary>
     public AudioMixer audioMixer;
+    
+    /// <summary>
+    /// Ссылка на выпадающий список доступных разрешений экрана.
+    /// </summary>
     public TMP_Dropdown resolutionsDropdown;
+
     private Resolution[] _resolutions;
 
     private void Start()
@@ -30,37 +41,65 @@ public class Menu : MonoBehaviour
         resolutionsDropdown.value = currentResolutionIndex;
         resolutionsDropdown.RefreshShownValue();
     }
-
+    
     public void ButtonCharacterPressed()
     {
         // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    /// <summary>
+    /// Обработка нажатия кнопки "Настройки". Переадресует пользователя на экран настроек.
+    /// </summary>
     public void ButtonSettingsPressed()
     {
         SceneManager.LoadScene(1);
     }
-
+    
+    /// <summary>
+    /// Обработка нажатия кнопки "Вернуться в меню". Переадресует пользователя на экран главного меню.
+    /// </summary>
     public void ButtonToMenuPressed()
     {
         SceneManager.LoadScene(0);
     }
-
+    
+    /// <summary>
+    /// Обработка нажатия кнопки "Выйти из игры". Закрывает окно программы и прекращает процесс.
+    /// </summary>
     public void ButtonExitPressed()
     {
         Application.Quit();
     }
 
+    /// <summary>
+    /// Обработка изменения значения слайдера "Громкость". Меняет громкость звука в программе.
+    /// </summary>
+    /// <param name="value">
+    /// Значение громкости.
+    /// </param>
     public void SliderVolumeChanged(float value)
     {
         audioMixer.SetFloat("volume", value);
     }
 
+    /// <summary>
+    /// Обработка переключателя "Полный экран". Переключает между режимами "Полный экран" и "Оконный режим".
+    /// </summary>
+    /// <param name="value">
+    /// True - режим "Полный экран".
+    /// False - "Оконный режим".
+    /// </param>
     public void ToggleFullScreenChanged(bool value)
     {
         Screen.fullScreen = value;
     }
 
+    /// <summary>
+    /// Обработка выбора в выпадном меню "Разрешение экрана". Устанавливает выбранное разрешение для приложения.
+    /// </summary>
+    /// <param name="index">
+    /// Индекс значения разрешения в выпадном списке.
+    /// </param>
     public void DropdownResolutionsChanged(int index)
     {
         var resolution = _resolutions[index];
